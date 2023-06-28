24119a272c5828ed31f6ee532fb076db55c90bcc
adamFittlerGit <137750965+adamFittlerGit@users.noreply.github.com>
2023-06-28 18:36:00 +1000

    Developer description added

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,7 +5,19 @@ import { ComputersCanvas } from './canvas';
 
 const Hero = () => {
   return (
-    <section className ="relative w-full h-screen mx-auto"></section>
+    <section className ="relative w-full h-screen mx-auto">
+      <div className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}>
+        <div className="flex flex-col justify-center items-center mt-5">
+          <div className="w-5 h-5 rounded-full bg-[#915eff]"/>
+          <div className="w-1 sm:h-80 h-40 violet-gradient"/>
+        </div>
+        <div>
+          <h1 className={`${styles.heroHeadText} text-white`}>Hi, I'm <span className="text-[#915eff]">Adam</span></h1>
+          <p className={`${styles.heroSubText} mt-2 text-white-100`}>I am a cybersecurity and machine learning specialist, focusing on providing efficient and effective algorithms as well as ensuring the safety and integrity of user data  </p>
+        </div>
+      </div>
+      <ComputersCanvas />
+    </section>
   )
 }
 
