e626c0a7c10dd43683dee3c980211bc25989422b
adamFittlerGit <137750965+adamFittlerGit@users.noreply.github.com>
2023-06-29 22:10:38 +1000

    Removed Link
    remvoed the little moving link thingy from the hero section I feel it was annoying and conflicted with the design too much

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -26,7 +26,7 @@ const Hero = () => {
       </div>
 
       <ComputersCanvas />
-
+      {/*
       <div className='relative xs:bottom-0 bottom-32 w-full flex justify-center items-center'>
         <a href='#about'>
           <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
@@ -44,6 +44,7 @@ const Hero = () => {
           </div>
         </a>
       </div>
+      */}
     </section>
   );
 };
