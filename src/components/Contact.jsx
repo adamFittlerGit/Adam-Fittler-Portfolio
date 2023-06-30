279572ebc81e9c09de1be6f2ddbe3ef610eca9e8
adamFittlerGit <137750965+adamFittlerGit@users.noreply.github.com>
2023-06-30 10:18:28 +1000

    Navbar updated

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -20,10 +20,12 @@ const Contact = () => {
   const handleChange = (e) => {}
 
   const handleSubmit = (e) => {}
-  
+
   return (
-    <div>Contact</div>
+    <div className="xl:mt-12 xl:flex-row flex-col-reverse flex gap-10 overflow-hidden">
+
+    </div>
   )
 }
 
-export default Contact
+export default SectionWrapper(Contact,"contact")
